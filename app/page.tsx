1330086c691fe0061fcf5e77a2515dcf1d422a41
Arhon Pineda <apineda@wsfcu.com>
2025-12-19 16:52:41 -0600

    create landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,49 @@ import {
   IconCheck,
 } from "@tabler/icons-react";
 
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main>
+        {/* hero */}
+        <section className="mt-20 h-1/2 px-6 max-w-5xl mx-auto flex flex-col">
+          <h1 className="leading-tight mb-2 text-balance">
+            This Is The Problem Stack
+          </h1>
+          <p>
+            A directory of problems and solutions designed to fuel your next
+            software project. Built for developers to collaborate, innovate, and
+            grow together.
+          </p>
+          <br />
+          {/* mechanics */}
+          <ProcessFlow />
+          <Divider />
+          <h2>Build With Purpose</h2>
+          <div className="sm:inline-flex gap-4 items-center">
+            <p>
+              Stop building to ship and start building to solve. Innovation
+              lives in the messy, unsolved problems, not in the tenth iteration
+              of a weather app. Find a problem that matters and own the
+              solution!
+            </p>
+            <br />
+            <button className="bg-black text-nowrap from-primary h-max from-50% bg-linear-30 to-white to-110% text-white rounded-lg p-2 w-max shadow-lg md:mx-auto">
+              Explore Now
+            </button>
+          </div>
+        </section>
+        {/* testimonials */}
+        <section></section>
+        {/* call to action */}
+        <section></section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
 const ProcessFlow = () => {
   const steps = [
     {
@@ -44,7 +87,7 @@ const ProcessFlow = () => {
   ];
 
   return (
-    <section className="w-full px-6">
+    <section className=" max-w-5xl mx-auto">
       <h2>How It Works</h2>
       <br />
       <div className="max-w-6xl mx-auto">
@@ -73,8 +116,7 @@ const ProcessFlow = () => {
                   {step.phase}
                 </p>
                 <h3 className="text-lg font-bold leading-snug">{step.title}</h3>
-                <div className="w-8 h-0.5 bg-primary"></div>{" "}
-                {/* divider */}
+                <div className="w-8 h-0.5 bg-primary"></div> {/* divider */}
                 <p className="text-sm contrast-50 leading-relaxed">
                   {step.description}
                 </p>
@@ -87,40 +129,8 @@ const ProcessFlow = () => {
   );
 };
 
-export default function Home() {
+function Divider() {
   return (
-    <>
-      <Header />
-      <main>
-        {/* hero */}
-        <section className="mt-20 h-1/2 px-6 max-w-5xl mx-auto flex flex-col">
-          <h1 className="leading-tight mb-2 text-balance">
-            This Is The Problem Stack
-          </h1>
-          <p>
-            A directory of problems and solutions designed to fuel your next
-            software project. Built for developers to collaborate, innovate, and
-            grow together.
-          </p>
-          <br />
-          <button className="bg-black from-primary from-50% bg-linear-30 to-white to-110% text-white rounded-lg p-2 w-max shadow-lg">Explore Now</button>
-          <br />
-          <h2>Build With Purpose</h2>
-          <p>
-            Stop building to ship and start building to solve. Innovation lives
-            in the messy, unsolved problems, not in the tenth iteration of a
-            weather app. Find a problem that matters and own the solution!
-          </p>
-        </section>
-        <hr className="my-10 mx-6 text-muted" />
-        {/* features */}
-        <ProcessFlow />
-        {/* testimonials */}
-        <section></section>
-        {/* call to action */}
-        <section></section>
-      </main>
-      <Footer />
-    </>
+    <hr className="my-5 text-muted mx-6 sm:mx-40 opacity-0" />
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 function Footer() {
   return (
-    <footer className="text-center p-4">&copy; {new Date().getFullYear()} The Problem Stack</footer>
+    <footer className="text-center p-4 bg-foreground text-background grow place-content-center mt-10">
+      <p>&copy; {new Date().getFullYear()} The Problem Stack</p>
+    </footer>
   )
 }
 
